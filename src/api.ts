0c66d972e22e053adbec32985fa64e5cd809cb35
Vladyslav Shylin <indiggoo228@gmail.com>
2024-12-04 16:16:16 +0200

    2

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://indigo04.github.io/chat-app/api'
+axios.defaults.baseURL = 'https://indigo04.github.io/chat-app/api/index.js'
 axios.defaults.withCredentials = true
 
 type AuthProps = {
